fbc41253ee38eb156ec8fc1b51776d486216e480
Ashish Verma <ashish.verma@excelli.com>
2022-03-02 21:57:02 +0530

    Network Layer Code Added
    This is a commit after so long.

---
+++ DXMindsDemo/DXMindsDemo/NetworkClasses/EndPoint.swift
@@ -0,0 +1,12 @@
+//
+//  EndPoint.swift
+//  DXMindsDemo
+//
+//  Created by Ashish Viltoriya on 03/05/21.
+//
+
+import Foundation
+
+struct Endpoint {
+    static let userDetails = "https://reqres.in/api/users?page=1"
+}
